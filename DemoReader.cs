d5556c7a297463fb63e7911a88babdb5c7a11a63
MyDimeIsUp <email@example.com>
2021-01-01 20:13:13 -0500

    Add project files.

---
+++ DemoReader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// CUSTOM SYSTEM IMPORTS
+using System.IO;
+
+namespace SeriousTTT_Demo_Reviewer
+{
+    public class DemoReader
+    {
+        private const string SOURCE_ENGINE_DEMO_HEADER = "GMODEMO";
+        private const int SOURCE_ENGINE_DEMO_PROTOCOL = 3;
+        private readonly BinaryReader _binaryReader;
+        public DemoHeader Header;
+
+        public DemoReader(BinaryReader binaryReader)
+        {
+            _binaryReader = binaryReader;
+        }
+
+        public DemoReader(string path)
+        {
+            var input = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            _binaryReader = new BinaryReader(input);
+        }
+
+        public void ReadHeader()
+        {
+            Header = new DemoHeader();
+            Header.Header = StringUtils.ReadNullTerminatedString(_binaryReader);
+            if (Header.Header != SOURCE_ENGINE_DEMO_HEADER)
+            {
+                throw new FileLoadException($"Demo header '{Header.Header}' is not equal to '{SOURCE_ENGINE_DEMO_HEADER}'!");
+            }
+
+            Header.DemoProtocol = _binaryReader.ReadInt32();
+            if (Header.DemoProtocol != SOURCE_ENGINE_DEMO_PROTOCOL)
+            {
+                throw new FileLoadException($"Demo protocol '{Header.DemoProtocol}' is not equal to '{SOURCE_ENGINE_DEMO_PROTOCOL}'!");
+            }
+
+            Header.NetworkProtocol = _binaryReader.ReadInt32();
+            Header.ServerName = StringUtils.ReadSizedNullString(_binaryReader, 260);
+            Header.ClientName = StringUtils.ReadSizedNullString(_binaryReader, 260);
+            Header.MapName = StringUtils.ReadSizedNullString(_binaryReader, 260);
+            Header.GameDirectory = StringUtils.ReadSizedNullString(_binaryReader, 260);
+            Header.PlaybackTime = _binaryReader.ReadSingle();
+            Header.Ticks = _binaryReader.ReadInt32();
+            Header.Frames = _binaryReader.ReadInt32();
+            Header.SignonDataLength = _binaryReader.ReadInt32();
+        }
+
+        public class DemoHeader
+        {
+            public string Header { get; set; }
+            public int DemoProtocol { get; set; }
+            public int NetworkProtocol { get; set; }
+            public string ServerName { get; set; }
+            public string ClientName { get; set; }
+            public string MapName { get; set; }
+            public string GameDirectory { get; set; }
+            public float PlaybackTime { get; set; }
+            public int Ticks { get; set; }
+            public int Frames { get; set; }
+            public int SignonDataLength { get; set; }
+            public int TickRate => (int)Math.Floor(Ticks / PlaybackTime);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+// CUSTOM SYSTEM IMPORTS
+using System.IO;
+using System.Collections.Generic;
+using System;
+using Microsoft.Win32;
+
+namespace SeriousTTT_Demo_Reviewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Server IP -> Name
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void processDemo(string sPath)
+        {
+            Dictionary<string, string> serverIpToName = new Dictionary<string, string>();
+
+            // Regular IPs
+            serverIpToName["192.184.57.2:27015"] = "Serious TTT West";
+            serverIpToName["172.106.104.11:27015"] = "Serious TTT East";
+            serverIpToName["192.184.57.6:27015"] = "Serious TTT Vanilla";
+            serverIpToName["172.106.104.13:27015"] = "Serious TTT Vanilla 2";
+            serverIpToName["172.106.104.14:27015"] = "SGM Deathrun East";
+
+            // URL redirects
+            serverIpToName["west.seriousttt.com"] = "Serious TTT West";
+            serverIpToName["east.seriousttt.com"] = "Serious TTT East";
+            serverIpToName["vanilla.seriousttt.com"] = "Serious TTT Vanilla";
+            serverIpToName["vanilla2.seriousttt.com"] = "Serious TTT Vanilla 2";
+            serverIpToName["east.seriousdr.com"] = "SGM Deathrun East";
+
+            if (!File.Exists(sPath))
+            {
+                MessageBox.Show("The demo file could not be found (File not found)", "ERROR: File not found", MessageBoxButton.OK, MessageBoxImage.Error);
+                return; // Stop!
+            }
+
+            var demoReader = new DemoReader(sPath);
+            demoReader.ReadHeader();
+
+            Console.WriteLine(demoReader.Header.Header);
+
+            string sServerName;
+
+            if (serverIpToName.ContainsKey(demoReader.Header.ServerName))
+            {
+                sServerName = serverIpToName[demoReader.Header.ServerName];
+            } else
+            {
+                sServerName = demoReader.Header.ServerName + " | (ERROR RESOLVING IP TO SERIOUS GMOD SERVER NAME";
+            }
+
+            demoFileInfo.Text = string.Format("Demo Start: {0}\nDemo End: {1}\nServer: {2}\nMap: {3}", File.GetCreationTime(sPath), File.GetLastWriteTime(sPath), sServerName, demoReader.Header.MapName);
+
+            MessageBox.Show("Demo loaded and displayed");
+        }
+
+        /// <summary>
+        /// Take input of demo file and read header of demo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void demoFileName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                processDemo(demoFileName.Text);
+            }
+        }
+
+        private void selectDemoFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Filter = "Demo files |*.dem";
+
+            Nullable<bool> result = fileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string sFilePath = fileDialog.FileName;
+                demoFileName.Text = sFilePath;
+                processDemo(sFilePath);
+            }
+        }
+    }
+}
